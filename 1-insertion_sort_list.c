6492df8a22762951cf92518b7f20e5fb45f27ac9
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-24 20:24:04 +0100

    done with 1-O and 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+
+/**
+* swap_nodes - swap two nodes in a doubly
+* linked list
+* @a: listint_t node
+* @b: listint_t node
+*/
+void swap_nodes(listint_t **a, listint_t **b)
+{
+	listint_t *next, *prev;
+
+	next = (*b)->next;
+	prev = (*a)->prev;
+	(*b)->next = *a;
+	(*b)->prev = prev;
+	if (prev)
+		prev->next = *b;
+	(*a)->next = next;
+	if (next)
+		next->prev = *a;
+	(*a)->prev = *b;
+}
+
+/**
+* insertion_sort_list - insertion sort algorithm to
+* sort a doubly linked list
+* @list: head of the linked list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *first, *second, *tmp;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	curr = *list;
+	while (curr)
+	{
+		first = curr;
+		second = curr->next;
+		if (first && second && first->n > second->n)
+		{
+			swap_nodes(&first, &second);
+			if (second && second->prev == NULL)
+				*list = second;
+			print_list(*list);
+			while (second && second->prev && second->n < second->prev->n)
+			{
+				tmp = second->prev;
+				swap_nodes(&tmp, &second);
+				if (second->prev == NULL)
+					*list = second;
+				print_list(*list);
+			}
+		}
+		else
+			curr = curr->next;
+	}
+}
